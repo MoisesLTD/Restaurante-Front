21addf3d63aa73beb333002174e356b4ce900a66
MoisesLaTorre <moises.latorre.davila@gmail.com>
2021-04-17 16:48:06 -0500

    Primer commit

---
+++ src/app/infrastructure/constans/Path.ts
@@ -0,0 +1,4 @@
+export class Path {
+  public static logo = "../../logo_oficial.png";
+  public static loading = "../../../assets/cargando.gif";
+}
---
+++ src/app/presentacion/presentacion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-presentacion",
+  templateUrl: "./presentacion.component.html",
+  styleUrls: ["./presentacion.component.css"],
+})
+export class PresentacionComponent {}
